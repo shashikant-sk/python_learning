2ac110dbf855e6bf638b3b92f15ed1353a092b5b
shashikant <shashikantshah540@gmail.com>
2022-02-05 01:48:52 +0530

    nested if statement

---
+++ Constructors in Python/if else/nested_if_std.py
@@ -0,0 +1,12 @@
+# Syntax  of nested-if statements
+
+# if (condition1):
+#     #executes if condition1 is True 
+#     if (condition2):
+#         #executes if condition 2 is true 
+#     #condition2 end here
+# #condition1 end here
+
+
+
+
