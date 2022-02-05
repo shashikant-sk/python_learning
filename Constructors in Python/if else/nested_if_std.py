f5891af5256f9c2e56b87f6c7d43a20d24778071
shashikant <shashikantshah540@gmail.com>
2022-02-05 18:51:23 +0530

    nested if statement

--- Constructors in Python/if else/nested_if_std.py
+++ Constructors in Python/if else/nested_if_std.py
@@ -8,5 +8,11 @@
 # #condition1 end here
 
 
-
-
+c = 21
+if c<25:
+    if c%2==0:
+        print("C is an even number less than 25")
+    else:
+        print("c is an odd number less than 25")
+else:
+    print("c is greater than 25")
