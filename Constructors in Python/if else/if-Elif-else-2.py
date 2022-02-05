11e7f638741eb142c2e22fccc27259c4b3aab191
shashikant <shashikantshah540@gmail.com>
2022-02-05 21:57:42 +0530

    IF Elif Else statement testing Vowel and consonant
    testing a,e,i,o,u and consonant value in if elif else statement

---
+++ Constructors in Python/if else/if-Elif-else-2.py
@@ -0,0 +1,17 @@
+var = "a"
+if var=='a':
+    print("This is the vowel a")
+elif var=='e':
+    print("This is the vowel e")
+elif var=='i':
+    print("This is the vowel i")
+elif var=='o':
+    print("This is the vowel o")
+elif var=='u':
+    print("This is the vowel u")
+else:
+    print("This is consonant")
+        
+    
+    
+        
