9ad4000676f7b14a7125e63bacb12d4a62e0070f
shashikant <shashikantshah540@gmail.com>
2022-02-07 22:11:23 +0530

    nested loops

--- loops in python/nested_loops.py
+++ loops in python/nested_loops.py
@@ -1,1 +1,9 @@
 #  nested loops 
+#  python allows loops to be nested in loops , Nested loops could be :
+
+
+# for in for i      |  while in while 
+# for var1 in seq1  | while condition(1) 
+# for var2 in seq2  | while condition(2)
+# statemnet(s)      | statemnet(s)          
+
