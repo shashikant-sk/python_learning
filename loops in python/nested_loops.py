3b74945f03b90e6ff56bff46d539c38a4328bd18
shashikant <shashikantshah540@gmail.com>
2022-02-07 22:16:24 +0530

    nested loops

--- loops in python/nested_loops.py
+++ loops in python/nested_loops.py
@@ -6,3 +6,9 @@
 # for var1 in seq1  | while condition(1) 
 # for var2 in seq2  | while condition(2)
 # statemnet(s)      | statemnet(s)          
+# ---------------------------------------------------------------
+# for in while         | while in for
+# while condition(1)   | for var1 in seq1
+# for var1 in seq1     | while condition
+# statements(s)        | statements(s)
+# -----------------------------------------------
