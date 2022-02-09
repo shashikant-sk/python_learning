f70eb612323208e75863f6cf25567fd58a985993
shashikant <shashikantshah540@gmail.com>
2022-02-09 20:35:16 +0530

    8 boll pull game

--- loop control statements/break_statements.py
+++ loop control statements/break_statements.py
@@ -5,3 +5,4 @@ for i in x :
     if i ==".": 
         break
     print(i,end=" ")
+    
---
+++ loop control statements/continue_statements.py
@@ -0,0 +1,7 @@
+# continue :- skips the statements following it and returns control to the Beginning of the loops
+
+for i in [1,2,3,55,66,77] :
+    if i>10:
+        continue
+    print(i)
+    
