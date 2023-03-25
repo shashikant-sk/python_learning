8b3b515d80fff20e3477d17153872a806d5e3eca
shashikant-sk <shashikantshah540@gmail.com>
2023-03-25 22:37:15 +0530

    Create insert_method.py

---
+++ python programming/insert_method.py
@@ -0,0 +1,9 @@
+# what if we want to insert an element somewhere in between the list for this we use
+
+num = [1,3,4,5,6,7,9]
+print(num)
+ 
+num.insert(1,2) #--> it add the value in the index 1
+num.insert(7,8) #--> it add the value in the last of the list
+ 
+print(num)
