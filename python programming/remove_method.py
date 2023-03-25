9e84163f4b2e6e0b1409662a03e3cfdaf6557934
shashikant-sk <shashikantshah540@gmail.com>
2023-03-25 23:27:16 +0530

    Create remove_method.py

---
+++ python programming/remove_method.py
@@ -0,0 +1,10 @@
+# remove method is used to remove the element from the list
+# it basically removes the first occurence of the element from the list that particular element
+
+
+# syntax 
+ 
+#   varial_name.remove(element)
+#   print(varial_name)
+  
+  
